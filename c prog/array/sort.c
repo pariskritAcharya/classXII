4f93b20e5e951b847f8de2cf3ffd72ba1aeb4a39
pariskritAcharya <pariskritacharya9999@gmail.com>
2025-03-08 16:57:55 +0545

    added new file

--- c prog/array/sort.c
+++ c prog/array/sort.c
@@ -1,44 +1,54 @@
-#include <stdio.h>
-#include <string.h>
+#include<stdio.h>
+#include<string.h>
 
 // sort int using function pointer
-int* sort(int* a, int len){
-    for(int i=0;i<len-1;i++){
-        for(int j=i+1;j<len;j++){
-            if( *(a+i)> *(a+j)){
-                int temp= *(a+i);
-                *(a+i)=*(a+j);
-                *(a+j)=temp;
+int *sort(int *a, int len)
+{
+    for (int i = 0; i < len - 1; i++)
+    {
+        for (int j = i + 1; j < len; j++)
+        {
+            if (*(a + i) > *(a + j))
+            {
+                int temp = *(a + i);
+                *(a + i) = *(a + j);
+                *(a + j) = temp;
             }
         }
     }
 
-return a;
+    return a;
 }
 
-
-//sort char using function pointer
-char* sortc(char* a, int len){
-    for(int i=0;i<len-1;i++){
-        for(int j=i+1;j<len;j++){
-              if( *(a+i)> *(a+j)){
-                int temp= *(a+i);
-                *(a+i)=*(a+j);
-                *(a+j)=temp;
+// sort char using function pointer
+char *sortc(char *a, int len)
+{
+    for (int i = 0; i < len - 1; i++)
+    {
+        for (int j = i + 1; j < len; j++)
+        {
+            if (*(a + i) > *(a + j))
+            {
+                int temp = *(a + i);
+                *(a + i) = *(a + j);
+                *(a + j) = temp;
             }
         }
     }
 
-return a;
+    return a;
 }
 
-
-//sort char using function pointer
-void sorts(char* a[], int len) {
-    for (int i = 0; i < len - 1; i++) {
-        for (int j = i + 1; j < len; j++) {
-            if (strcmp(a[i], a[j]) > 0) {
-                char* temp = a[i];
+// sort char using function pointer
+void sorts(char *a[], int len)
+{
+    for (int i = 0; i < len - 1; i++)
+    {
+        for (int j = i + 1; j < len; j++)
+        {
+            if (strcmp(a[i], a[j]) > 0)
+            {
+                char *temp = a[i];
                 a[i] = a[j];
                 a[j] = temp;
             }
@@ -46,48 +56,41 @@ void sorts(char* a[], int len) {
     }
 }
 
+int main()
+{
 
+    // declare array and their length
+    int i[] = {6, 5, 3, 2, 4, 1};
+    int len = sizeof(i) / sizeof(i[0]);
 
-int main(){
+    char c[] = {'e', 'f', 'd', 'c', 'b', 'a'};
+    int lenc = sizeof(c) / sizeof(c[0]);
 
+    char *s[] = {"apple", "ball", "cat", "ape", "doll", "ball"};
+    int lens = sizeof(s) / sizeof(s[0]);
 
-//declare array and their length
-int i[]={6,5,3,2,4,1};
-int len = sizeof(i)/sizeof(i[0]);
+    int *ic = sort(i, len);
+    char *cc = sortc(c, lenc);
+    sorts(s, lens);
 
-char c[]={'e','f','d','c','b','a'};
-int lenc = sizeof(c)/sizeof(c[0]);
+    // outputs
+    printf("\nsorted int\n");
+    for (int i = 0; i < len; i++)
+    {
+        printf(" %d ", *(ic + i));
+    }
 
-char* s[]={"apple","ball","cat","ape","doll","ball"};
-int lens=sizeof(s)/sizeof(s[0]);
+    printf("\nsorted char\n");
+    for (int i = 0; i < len; i++)
+    {
+        printf(" %c ", *(cc + i));
+    }
 
+    printf("\nsorted string\n");
+    for (int i = 0; i < len; i++)
+    {
+        printf(" %s ", s[i]);
+    };
 
-
-int* ic=sort(i,len);
-char* cc=sortc(c,lenc);
-sorts(s,lens);
-
-//outputs
-printf("\nsorted int\n");
-for(int i=0;i<len;i++){
-    printf(" %d ",*(ic+i));
+    return 0;
 }
-
-
- printf("\nsorted char\n");
-for(int i=0;i<len;i++){
-    printf(" %c ",*(cc+i));
-}
-
-printf("\nsorted string\n");
-for(int i=0;i<len;i++){
-    printf(" %s ",s[i]);
-};
-
-
-
-return 0;
-}
-
-
-
---
+++ c prog/array/sortInt.c
@@ -0,0 +1,151 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void print(int a[], int n);
+void sort_function(int a[], int n);
+void sort_pointer(int *a,int n);
+int main()
+{
+    int n;
+    printf("enter total number you want to enter:");
+    scanf("%d", &n);
+    int a[n];
+
+    // input array of numbers
+    for (int i = 0; i < n; i++)
+    {
+        switch (i+1)
+        {
+        case 1:
+            printf("enter 1st number");
+            break;
+        case 2:
+            printf("enter 2nd number");
+            break;
+        case 3:
+            printf("enter 3rd number");
+            break;
+        default:
+            printf("\nenter %dth number:", i+1);
+            break;
+        }
+        scanf("%d", &a[i]);
+    }
+    system("cls");
+    printf("initial numbers:");
+    print(a, n);
+
+
+    //three copies of a 
+    int a_cpy1[n];
+    memcpy(a_cpy1, a, n * sizeof(a[0]));
+
+    int a_cpy2[n];
+    memcpy(a_cpy2, a, sizeof(a));
+
+    int a_cpy3[n]; 
+    for (int i = 0; i < n; i++){
+        a_cpy3[i]=a[i];
+    }
+
+    // bubble sorting by loop
+
+
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (a_cpy1[i] > a_cpy1[j])
+            {
+                a_cpy1[i] += a_cpy1[j];
+                a_cpy1[j] = a_cpy1[i] - a_cpy1[j];
+                a_cpy1[i] = a_cpy1[i] - a_cpy1[j];
+            }
+        }
+    }
+
+    printf("\nrequired sorted form from loop is:");
+    print(a_cpy1, n);
+
+    // bubble sorting by function
+    sort_function(a_cpy2, n);
+
+    // bubble sorting by pointer
+    sort_pointer(a_cpy3, n);
+    printf("\nrequired sorted form from pointer function is:");
+    print(a_cpy3, n);
+    printf("\n");
+    
+    return 0;
+}
+
+
+
+
+//functions
+void print(int a[], int n)
+{
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+}
+
+
+
+//sorting with function
+void sort_function(int a[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (a[i] < a[j])
+            {
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+
+    printf("\nrequired descending from function is:");
+
+    print(a, n);
+}
+
+
+
+
+//sorting using pointer function
+void sort_pointer(int *a,int n){
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            if (*(a+i) > *(a+j))
+            {
+                int temp = *(a+i);
+                *(a+i) = *(a+j);
+                *(a+j) = temp;
+            }
+        }
+    }
+
+    
+}
+
+
+/*
+
+output:
+initial numbers:2 3 1 4 
+required sorted form from loop is:1 2 3 4 
+required descending from function is:4 3 2 1 
+required sorted form from pointer function is:1 2 3 4 
+
+
+*/
